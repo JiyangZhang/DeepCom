9f77229f9f35354f12cb97531d0fa14f7692e94b
JiyangZhang <jiyang.zhang@utexas.edu>
2020-08-09 17:14:44 -0500

    Update the way to calculate Bleu.

--- translate/Bleu.py
+++ translate/Bleu.py
@@ -1,22 +1,33 @@
 from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
 from seutil import LoggingUtils, BashUtils, IOUtils
-from csevo.Environment import Environment
-
+import sys
 
 class Bleu:
 
-    logger = LoggingUtils.get_logger(__name__, LoggingUtils.DEBUG if Environment.is_debug else LoggingUtils.INFO)
+    #logger = LoggingUtils.get_logger(__name__, LoggingUtils.DEBUG if Environment.is_debug else LoggingUtils.INFO)
 
     @classmethod
-    def compute_bleu(cls, references, hypotheses):
+    def compute_bleu(cls, references:str, hypotheses: str, exp: str) -> int:
         with open(references, 'r') as fr, open(hypotheses, 'r') as fh:
             refs = fr.readlines()
             hyps = fh.readlines()
         bleu_4_sentence_scores = []
         for ref, hyp in zip(refs, hyps):
-            bleu_4_sentence_scores.append(sentence_bleu([ref.strip().split()], hyp.strip().split(),
+            if len(hyp.strip().split()) < 2:
+                bleu_4_sentence_scores.append(0)
+            else:
+                bleu_4_sentence_scores.append(sentence_bleu([ref.strip().split()], hyp.strip().split(),
                                                         smoothing_function=SmoothingFunction().method2,
                                                         auto_reweigh=True))
         score = 100 * sum(bleu_4_sentence_scores) / float(len(bleu_4_sentence_scores))
-        cls.logger.info(f"BLEU-4 score is: {score}")
-        return
+        print(score)
+        bleu_result_file = f"{exp}/bleu.json"
+        result = {"Bleu": score}
+        IOUtils.dump(bleu_result_file, result)
+        return score
+
+if __name__ == "__main__":
+    ref_file = sys.argv[1]
+    hyp_file = sys.argv[2]
+    exp_dir = sys.argv[3]
+    Bleu.compute_bleu(ref_file, hyp_file, exp_dir)
