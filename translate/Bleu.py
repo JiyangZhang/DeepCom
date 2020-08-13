5ac56ac23ac4ea6b5970c16bdd5f28141a0e8781
JiyangZhang <jiyang.zhang@utexas.edu>
2020-08-13 14:58:06 -0500

    Update Bleu.

--- translate/Bleu.py
+++ translate/Bleu.py
@@ -1,11 +1,9 @@
 from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
 from seutil import LoggingUtils, BashUtils, IOUtils
-from csevo.Environment import Environment
 
 
 class Bleu:
 
-    logger = LoggingUtils.get_logger(__name__, LoggingUtils.DEBUG if Environment.is_debug else LoggingUtils.INFO)
 
     @classmethod
     def compute_bleu(cls, references, hypotheses):
@@ -14,9 +12,14 @@ class Bleu:
             hyps = fh.readlines()
         bleu_4_sentence_scores = []
         for ref, hyp in zip(refs, hyps):
-            bleu_4_sentence_scores.append(sentence_bleu([ref.strip().split()], hyp.strip().split(),
+            if len(hyp.strip().split()) < 2:
+                bleu_4_sentence_scores.append(0)
+            else:
+                bleu_4_sentence_scores.append(sentence_bleu([ref.strip().split()], hyp.strip().split(),
                                                         smoothing_function=SmoothingFunction().method2,
                                                         auto_reweigh=True))
         score = 100 * sum(bleu_4_sentence_scores) / float(len(bleu_4_sentence_scores))
-        cls.logger.info(f"BLEU-4 score is: {score}")
+        results = {"Bleu": score}
+        IOUtils.dump()
+
         return
